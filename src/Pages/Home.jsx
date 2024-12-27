5f0acaea136883b935d6e9a3ceb98482c784ade8
Sajid M Tariq <sajidmehmoodtariq5@gmail.com>
2024-12-27 16:22:30 +0500

    Banner section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import Hero from '../Components/Hero'
 import Category from '../Components/Category'
 import Services from '../Components/Services'
+import Banner from '../Components/Banner'
+import headphone from '../assets/hero/headphone.png'
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "2021-12-31",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptate.",
+  bgColor: "#f42c37"
+}
 
 const Home = () => {
   return (
@@ -9,6 +22,7 @@ const Home = () => {
       <Hero />
       <Category />
       <Services />
+      <Banner data = {BannerData}/>
     </div>
   )
 }
